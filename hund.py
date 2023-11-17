14dc4ecbc819b273abac970444440d5ff537e8b5
Lyx <130296296+Lyxminxx@users.noreply.github.com>
2023-11-17 11:25:56 +0100

    added two programs
    Just two programs that find the number of squares in a spesific figure.

---
+++ hund.py
@@ -0,0 +1,5 @@
+n = input("Skriv inn figurnummer ")
+n = int(n)
+f = n*(3*n+4)
+
+print ("Det er",f,"kuber i figuren din.")
---
+++ kvadrat.py
@@ -0,0 +1,5 @@
+x = input("Skriv sidelengden av kvadratet: ")
+x = float(x)
+svar = (x-1)*4
+print("Det er ", svar, " kuber i rammen av kuben.")
+
